3c4fe3bbcd4d26b4e9d99e34ed5928abe0e5dcfd
William Yu <wy427@nyu.edu>
2022-04-13 21:01:08 -0400

    Update membership.rb

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,7 +2,6 @@ class Membership < ApplicationRecord
   belongs_to :gym
   belongs_to :client
 
-  validates :gym_id, presence: true, uniqueness: true
-  validates :client_id, presence: true, uniqueness: true
+ validates :client_id, uniqueness: {scope: :gym_id}
   validates :charge, presence: true
 end
